2b8b5e657e98db92025e49148097571225b14f77
Ina Davidova <ina.davidovaa@gmail.com>
2023-07-10 01:24:49 +0300

    Added photo card for displaying the pictures

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotoCardComponent } from './photo-card/photo-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoCardComponent
   ],
   imports: [
     BrowserModule,
