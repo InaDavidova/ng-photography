707dcbfb1f2ebd5e6f2c37b4a9b17ba727514c3e
Ina Davidova <ina.davidovaa@gmail.com>
2023-08-02 00:57:48 +0300

    Styled Home page

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth.service';
+import { PostService } from 'src/app/core/post.service';
+import { IPostResponse } from 'src/app/interfaces/photoData';
+import { IUser } from 'src/app/interfaces/userData';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  data!: IPostResponse[];
+  isLoading!: boolean;
+  currentUser: IUser | undefined;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private postService: PostService,
+    private authService: AuthService
+  ) {
+    this.authService.currentUser$.subscribe(
+      (user) => (this.currentUser = user)
+    );
   }
 
+  ngOnInit(): void {
+    this.postService.getPosts$(3).subscribe((postList) => {
+      this.data = postList;
+      this.isLoading = false;
+    });
+  }
 }
